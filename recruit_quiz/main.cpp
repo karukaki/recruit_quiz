20995901b9734634de8182b2c43829796b39efe4
大輔 柳澤 <yanagizawadaisuke@gmail.com>
2024-10-15 18:23:54 +0900

    questionsのint aをstring形に変更

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -8,12 +8,12 @@ using namespace std;
 struct Question
 {
 	string q;
-	int a;
+	string a;
 };
 
 int main()
 {
-	vector<Question> questions(3);
+	vector<Question> questions(6);
 
 	random_device rd;
 	mt19937 rand(rd());
@@ -22,13 +22,13 @@ int main()
 	int x = uniform_int_distribution<>(1, 30)(rand);
 	int y = uniform_int_distribution<>(1, 20)(rand);
 	questions[0].q = to_string(x) + "x" + to_string(y) + "の答えは？";
-	questions[0].a = x * y;
+	questions[0].a = to_string(x * y);
 
 	//割り算
 	x = uniform_int_distribution<>(1, 30)(rand);
 	y = uniform_int_distribution<>(1, 20)(rand);
 	questions[1].q = to_string(x * y) + "÷" + to_string(y) + "の答えは？";
-	questions[1].a = x;
+	questions[1].a = to_string(x);
 
 	//複雑な式
 	x = uniform_int_distribution<>(1, 100)(rand);
@@ -37,23 +37,23 @@ int main()
 	int w = uniform_int_distribution<>(1, 10)(rand);
 	questions[2].q =
 		to_string(x) + "-(" + to_string(y * w) + "+" + to_string(z * w) + ")÷" + to_string(w) + "の答えは？";
-	questions[2].a = x - (y + z);
+	questions[2].a = to_string(x - (y + z));
 	
 	//三角形の面積
 	x = uniform_int_distribution<>(1, 10)(rand);
 	y = uniform_int_distribution<>(1, 5)(rand) * 2;
-	questions.push_back({ "面積" + to_string(x * y / 2) + "cm^2,底辺" + to_string(y) + "cmの三角形の高さを求めよ。",x });
+	questions[3].a = ("面積" + to_string(x * y / 2) + "cm^2,底辺" + to_string(y) + "cmの三角形の高さを求めよ。",x);
 
 	//円錐の体積
 	x = uniform_int_distribution<>(1, 10)(rand);
 	y = uniform_int_distribution<>(1, 5)(rand) * 3;
-	questions.push_back({ "底面の半径" + to_string(x) + "cm,高さ" + to_string(y) + "cmの円錐がある。\n" + 
-		"この円錐の体積をXπcm^3とする。Xの値を求めよ。",x * x * y / 3 });
+	questions[4].a = ("底面の半径" + to_string(x) + "cm,高さ" + to_string(y) + "cmの円錐がある。\n" + 
+		"この円錐の体積をXπcm^3とする。Xの値を求めよ。",x * x * y / 3);
 
 	//球の体積
 	x = uniform_int_distribution<>(1, 5)(rand) * 3;
-	questions.push_back({ "球の半径" + to_string(x) + "cmの球がある。\n" + 
-		"この球の体積をXπcm^3とする。Xの値を求めよ。",x * x * x * 4 / 3});
+	questions[5].a = ( "球の半径" + to_string(x) + "cmの球がある。\n" + 
+		"この球の体積をXπcm^3とする。Xの値を求めよ。",x * x * x * 4 / 3);
 
 	cout << "[リクルート対策クイズ]\n";
 
@@ -61,7 +61,7 @@ int main()
 	for (const auto& e : questions) {
 		cout << e.q << "\n";
 
-		int answer;
+		string answer;
 		cin >> answer;
 
 		if (answer == e.a) {
