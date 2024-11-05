ab269005d4bf40140ea62bdd9242610f453596a4
大輔 柳澤 <yanagizawadaisuke@gmail.com>
2024-11-05 18:08:30 +0900

    英単語追加

--- recruit_quiz/exam_english.cpp
+++ recruit_quiz/exam_english.cpp
@@ -25,6 +25,22 @@ QuestionList CreateEnglishWordExam()
 		{ "infrastructure", "インフラストラクチャ", "下部構造、基盤となる施設や設備" },
 		{ "knowledge", "ナレッジ", "知識" },
 		{ "credit", "クレジット", "信用" },
+		{ "routine", "ルーティン", "決まった手順、日課" },
+		{ "management", "マネジメント", "	管理、経営"},
+		{ "account", "アカウント", "会計、口座"},
+		{ "unique", "ユニーク", "	他に類を見ない、個性的な"},
+		{ "variety", "バラエティ", "	変化に富む、多様な" },
+		{ "schedule", "スケジュール", "	予定、計画"},
+		{ "agenda", "アジェンダ", "	課題、議題"},
+		{ "technology", "テクノロジー", "科学技術"},
+		{ "collaboration", "コラボレーション", "協力、共同事業、共同作業"},
+		{ "tax", "タックス", "税金"},
+		{ "stock", "ストック", "在庫、株券"},
+		{ "product", "プロダクト", "製品"},
+		{ "booking", "ブッキング", "予約、帳簿への記入"},
+		{ "weight", "ウェイト", "重さ"},
+		{ "compliance", "コンプライアンス", "法令・社会的規範を守る"},
+		{ "receipt", "レシート", "領収書"},
 		};
 		
 	constexpr int quizCount = 5;
@@ -38,8 +54,7 @@ QuestionList CreateEnglishWordExam()
 	switch (type) {
 	case 0: // 意味から英単語を答える
 
-
-	for (int i = 0; i < quizCount; i++) {
+		for (int i = 0; i < quizCount; i++) {
 		const auto & e = data[indices[i]];
 		questions.push_back({
 		"「" + string(e.meaning) + "」を意味する英単語を答えよ",
@@ -57,7 +72,7 @@ QuestionList CreateEnglishWordExam()
 			e.word });
 			
 		}
-	break;
+		break;
 
 	case 2: // 英単語から「読み」を答える
 		for (int i = 0; i < quizCount; i++) {
@@ -67,7 +82,29 @@ QuestionList CreateEnglishWordExam()
 			e.reading });
 			
 		}
-	break;
+		break;
+	
+	case 3: // 英単語の意味を答える
+		for (int i = 0; i < quizCount; i++) {
+			    // 間違った番号をランダムに選ぶ
+				const int correctIndex = indices[i];
+			vector<int> answers = CreateWrongIndices(size(data), correctIndex);
+			
+				 // ランダムな位置を正しい番号で上書き
+				const int correctNo = std::uniform_int_distribution<>(1, 3)(rd);
+			answers[correctNo - 1] = correctIndex;
+			
+			      // 問題文を作成
+				string s = "「" + string(data[correctIndex].word) +
+				"」の意味として正しい番号を選べ\n";
+			s += std::string("  1:") + data[answers[0]].meaning + "\n";
+			s += std::string("  2:") + data[answers[1]].meaning + "\n";
+			s += std::string("  3:") + data[answers[2]].meaning;
+			
+			questions.push_back({ s, to_string(correctNo) });
+			
+		}
+		break;
 	} // switch (type)
 
 	return questions;
